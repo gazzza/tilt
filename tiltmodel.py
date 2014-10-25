4c0705f311d6fe8c05566d08fd82db27011dd0fb
Gary <garyb.davies@gmail.com>
2014-10-25 18:48:39 +0100

    Added free energy closure to tilt model

--- tiltmodel.py
+++ tiltmodel.py
@@ -20,7 +20,40 @@ class Particle(object):
         return 4*np.pi*self.R_o**2*self.geometric_aspect
 
 
+class AnalyticalTiltModel(object):
+
+    def __init__(self,particle,surface_tension):
+        self.p = particle
+        self.gamma = surface_tension
+
+    def normalised_free_energy(self,unnormalised_b_field):
+        def closure(theta):
+            pref_term  = self.p.alpha/(4*self.p.geometric_aspect)
+            sqrt_term  = 1/np.sqrt(np.cos(theta)**2 + self.p.alpha**2*np.sin(theta)**2)
+            field_term = unnormalised_b_field*np.cos(np.pi/2 - theta)/(self.p.surface_area*self.gamma)
+            return -(pref_term*sqrt_term + field_term)
+        return closure
+
+    def relative_normalised_free_energy(self,unnormalised_b_field,angle):
+        free_energy_func = self.normalised_free_energy(unnormalised_b_field)
+        def wrapper(theta):
+            return free_energy_func(theta) - free_energy_func(angle)
+        return wrapper
+        
+    
+
+
 if __name__=="__main__":
 
     p = Particle(3.0,1.0)
     print p.surface_area
+
+    theta = np.arange(np.rad2deg(0),np.deg2rad(90),0.01)
+
+    tiltmodel = AnalyticalTiltModel(p,1)
+
+    b_field = 0
+    for b_field in [0,1,2,3,4,5]:
+        free_energy = tiltmodel.normalised_free_energy(b_field)
+        plt.plot(theta,free_energy(theta),'ro')
+    plt.show()
