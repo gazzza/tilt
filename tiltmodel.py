6c894407b972e0cbc1037245c4f364336bfecb66
Gary <garyb.davies@gmail.com>
2014-10-26 01:21:11 +0100

    Cleaning up

--- tiltmodel.py
+++ tiltmodel.py
@@ -19,6 +19,15 @@ class Particle(object):
     def surface_area(self):
         return 4*np.pi*self.R_o**2*self.geometric_aspect
 
+class Interface(object):
+    def __init__(self,x,y,surface_tension):
+        self.x = x
+        self.y = y
+        self.area = 2*self.x*self.y
+        self.gamma = surface_tension
+        
+    
+
 class AreaData(object):
     def __init__(self,path):
         self.file = path
@@ -32,26 +41,35 @@ class AreaData(object):
     def _angles(self):
         return self.data[:,0]
 
-    
     def tilt_angles(self,rad=False):
         if rad:
             return np.deg2rad(self._angles)
         else:
             return self._angles
 
-    
+    def area_removed(self,interface):
+        return interface.area - self.areas
+
+    def area_removed_norm(self,particle,interface):
+        norm =  particle.surface_area*interface.gamma
+        return self.area_removed(interface)/norm
+
+    def poly_area(self,order):
+        p_coeffs = np.polyfit(self.tilt_angles(rad=True),self.normalised_areas,order)
+        return np.poly1d(p_coeffs)
+        
 
 class AnalyticalTiltModel(object):
 
-    def __init__(self,particle,surface_tension):
+    def __init__(self,particle,interface):
         self.p = particle
-        self.gamma = surface_tension
+        self.I = interface
 
     def normalised_free_energy(self,unnormalised_b_field):
         def closure(theta):
             pref_term  = self.p.alpha/(4*self.p.geometric_aspect)
             sqrt_term  = 1/np.sqrt(np.cos(theta)**2 + self.p.alpha**2*np.sin(theta)**2)
-            field_term = unnormalised_b_field*np.cos(np.pi/2 - theta)/(self.p.surface_area*self.gamma)
+            field_term = unnormalised_b_field*np.cos(np.pi/2 - theta)/(self.p.surface_area*self.I.gamma)
             return -(pref_term*sqrt_term + field_term)
         return closure
 
@@ -64,6 +82,8 @@ class AnalyticalTiltModel(object):
 if __name__=="__main__":
 
     p = Particle(3.0,1.0)
+    I = Interface(40,20,1)
+
     print p.surface_area
 
     theta = np.arange(np.rad2deg(0),np.deg2rad(90),0.01)
@@ -81,3 +101,8 @@ if __name__=="__main__":
 
     d = AreaData('1p.txt')
     
+    A_rm = d.area_removed_norm(p,I)
+
+    plt.plot(theta,A_rm(theta),'ro')
+    plt.show()
+
